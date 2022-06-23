04e687eece212076920b7977fc5c937f32760adf
Amr Aboelela <amraboelela@gmail.com>
2022-06-22 17:38:22 -0700

    "#(searchText)"

--- Sources/App/Controllers/DarkEyeController.swift
+++ Sources/App/Controllers/DarkEyeController.swift
@@ -19,19 +19,13 @@ struct DarkEyeController: RouteCollection {
     }
     
     func searchHandler(_ req: Request) throws -> EventLoopFuture<View> {
-        if crawler.canRun {
-            crawler.canRun = false
-            DispatchQueue.main.asyncAfter(deadline: .now() + 60.0) {
-                Crawler.restart()
-            }
-        }
         let command = req.parameters.get("search") ?? ""
         if command != "search" {
             print("command != search")
         }
         let searchText = req.query[String.self, at: "text"] ?? ""
         print("searchText: \(searchText)")
-        let links = WordLink.wordLinks(withSearchText: searchText, count: DarkEyeModel.linksCount) //Link.links(withSearchText: searchText, count: DarkEyeModel.linksCount)
+        let links = WordLink.wordLinks(withSearchText: searchText, count: DarkEyeModel.linksCount)
         let darkEyeModel = DarkEyeModel.modelWith(req: req, title: "Dark Eye search: " + searchText, searchText: searchText, wordLinks: links)
         return req.view.render("darkeye", darkEyeModel)
     }
