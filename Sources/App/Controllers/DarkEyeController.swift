d6d2a5d7221068601c638ec3fb3d3ed17ee7becb
Amr Aboelela <amraboelela@gmail.com>
2022-07-14 16:23:36 -0700

    cleanup

--- Sources/App/Controllers/DarkEyeController.swift
+++ Sources/App/Controllers/DarkEyeController.swift
@@ -16,18 +16,10 @@ struct DarkEyeController: RouteCollection {
     // MARK: - route Handlers
     
     func darkEyeHandler(_ req: Request) -> EventLoopFuture<View> {
-        appController.stopTheApp()
-        defer {
-            crawler.start(after: 1)
-        }
         return req.view.render("darkeye", DarkEyeModel.modelWith(req: req, title: "Dark Eye", wordLinks: [WordLink]()))
     }
     
     func searchHandler(_ req: Request) throws -> EventLoopFuture<View> {
-        /*appController.stopTheApp()
-        defer {
-            crawler.start(after: 1)
-        }*/
         let command = req.parameters.get("search") ?? ""
         if command != "search" {
             print("command != search")
