3bd0c19c1a43f4919e98740629ec04367145b42b
Amr Aboelela <amraboelela@gmail.com>
2022-07-17 22:33:46 -1000

    if let title = link?.title, !title.isEmpty

--- Sources/App/Models/WordLinkModel.swift
+++ Sources/App/Models/WordLinkModel.swift
@@ -10,7 +10,11 @@ struct WordLinkModel: Codable {
     
     static func from(wordLink: WordLink) -> WordLinkModel {
         let link = wordLink.hashLink?.link
-        return WordLinkModel(url: link?.url ?? "", hash: wordLink.urlHash, title: link?.title ?? "", text: wordLink.text)
+        if let title = link?.title, !title.isEmpty {
+            return WordLinkModel(url: link?.url ?? "", hash: wordLink.urlHash, title: title, text: wordLink.text)
+        } else {
+            return WordLinkModel(url: "", hash: wordLink.urlHash, title: link?.url ?? "No Title", text: wordLink.text)
+        }
     }
     
     static func modelsWith(wordLinks: [WordLink], loggedInUser: User?) -> [WordLinkModel] {
