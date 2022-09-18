73e2b3d318342df155a3c12c94f66845d08d85c0
Amr Aboelela <amraboelela@gmail.com>
2022-09-18 16:00:30 -0700

    300

--- Sources/App/Models/DarkeyeModel.swift
+++ Sources/App/Models/DarkeyeModel.swift
@@ -6,7 +6,7 @@ import VaporCommon
 
 struct DarkeyeModel: Codable {
     static let title = "Darkeye legal search engine for DarkNet (the Dark Web)"
-    static let linksCount = 200
+    static let linksCount = 300
     
     var loggedInUser: User?
     var title: String
